8e790eccf64af6ade3b1de355fce03a87f109140
Mahfuj Salehin Moaz <almuaz1998@gmail.com>
2023-03-12 15:54:23 +0600

    main

---
+++ android/app/src/main/kotlin/com/assignment/wordpair_generator/MainActivity.kt
@@ -0,0 +1,17 @@
+// Mahfuz Salehin Moaz
+// ID : 183071007
+// Semester : 11th
+
+package com.assignment.wordpair_generator
+
+import android.os.Bundle
+
+import io.flutter.app.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
+
+class MainActivity: FlutterActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    GeneratedPluginRegistrant.registerWith(this)
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// Mahfuz Salehin Moaz
+// ID : 183071007
+// Semester : 11th
+
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        theme: ThemeData(primaryColor: Colors.purple[900]),
+        home: RandomWords());
+  }
+}
